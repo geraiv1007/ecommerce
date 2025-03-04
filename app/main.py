7e376909e4711d68c8708eb15c166c8062e210e7
gera <ivanovgerman93@mail.ru>
2025-03-04 21:28:27 +0300

    04.03.2025 celery remove

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-from celery import Celery
 from app.middleware.log import log_middleware
 from app.models.models import Base
 from app.routers import category, products, auth, reviews
@@ -12,11 +11,4 @@ app.include_router(category.router)
 app.include_router(products.router)
 app.include_router(auth.router)
 app.include_router(reviews.router)
-#app.middleware('http')(log_middleware)
-
-celery = Celery(
-    __name__,
-    broker='redis://127.0.0.1:6379/0',
-    backend='redis://127.0.0.1:6379/0',
-    broker_connection_retry_on_startup=True
-)
+app.middleware('http')(log_middleware)
